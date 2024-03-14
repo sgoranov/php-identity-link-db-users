90a8fb6073bff5d4eb58dbffc7399f041dd4de1c
Simeon Goranov <sgoranov@users.noreply.github.com>
2024-03-14 22:17:37 +0200

    Add firewall with JWT tokens

--- src/Security/User.php
+++ src/Security/User.php
@@ -2,32 +2,16 @@
 
 namespace App\Security;
 
-use Lexik\Bundle\JWTAuthenticationBundle\Security\User\JWTUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-final class User implements JWTUserInterface
+final class User implements UserInterface
 {
-    const ROLE_ADMIN_MAPPING = 'administrator';
 
     public function __construct(
         private readonly string $username,
-        private readonly array  $roles,
+        private readonly array $roles,
     )
     {
-    }
-
-    public static function createFromPayload($username, array $payload): JWTUserInterface|User
-    {
-        $roles = [];
-        if (isset($payload['groups']) && is_array($payload['groups']) &&
-            in_array(self::ROLE_ADMIN_MAPPING, $payload['groups'], true)) {
-
-            $roles[] = 'ROLE_ADMIN';
-        }
-
-        return new self(
-            $username,
-            $roles,
-        );
     }
 
     public function getRoles(): array
