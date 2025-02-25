e4fc975bcee367362eeb3fd5657fa608f3239b06
Simeon Goranov <sgoranov@users.noreply.github.com>
2025-02-25 18:29:09 +0200

    Add logging

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     sgoranov\PHPIdentityLinkShared\PHPIdentityLinkSharedBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
